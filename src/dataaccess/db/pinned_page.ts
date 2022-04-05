0bd9ad29acc6afe84ec46addcef919013f8355c6
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-04-05 21:49:35 +0800

    implement pinned page data accessor

--- src/dataaccess/db/pinned_page.ts
+++ src/dataaccess/db/pinned_page.ts
@@ -1,7 +1,8 @@
+import { status } from "@grpc/grpc-js";
 import { injected, token } from "brandi";
 import { Knex } from "knex";
 import { Logger } from "winston";
-import { LOGGER_TOKEN } from "../../utils";
+import { ErrorWithStatus, LOGGER_TOKEN } from "../../utils";
 import { KNEX_INSTANCE_TOKEN } from "./knex";
 
 export class PinnedPage {
@@ -31,8 +32,8 @@ export interface PinnedPageDataAccessor {
         offset: number,
         limit: number
     ): Promise<PinnedPage[]>;
-    getPinnedPage(id: number): Promise<PinnedPage>;
-    getPinnedPageWithXLock(id: number): Promise<PinnedPage>;
+    getPinnedPage(id: number): Promise<PinnedPage | null>;
+    getPinnedPageWithXLock(id: number): Promise<PinnedPage | null>;
     updatePinnedPage(pinnedPage: PinnedPage): Promise<void>;
     deletePinnedPage(id: number): Promise<void>;
 }
@@ -52,11 +53,39 @@ export class PinnedPageDataAccessorImpl implements PinnedPageDataAccessor {
     ) {}
 
     public async createPinnedPage(args: CreatePinnedPageArgs): Promise<number> {
-        throw new Error("Method not implemented.");
+        try {
+            const rows = await this.knex
+                .insert({
+                    [ColNamePinPageServicePinnedPageOfUserId]: args.ofUserId,
+                    [ColNamePinPageServicePinnedPagePinTime]: args.pinTime,
+                    [ColNamePinPageServicePinnedPageURL]: args.url,
+                    [ColNamePinPageServicePinnedPageScreenshotFilename]:
+                        args.screenshotFilename,
+                    [ColNamePinPageServicePinnedPageDescription]:
+                        args.screenshotFilename,
+                })
+                .returning([ColNamePinPageServicePinnedPagePinnedPageId])
+                .into(TabNamePinPageServicePinnedPage);
+            return +rows[0][ColNamePinPageServicePinnedPagePinnedPageId];
+        } catch (error) {
+            this.logger.error("failed to create pinned page", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
     }
 
     public async getPinnedPageCount(ofUserId: number): Promise<number> {
-        throw new Error("Method not implemented.");
+        try {
+            const rows = await this.knex
+                .count()
+                .from(TabNamePinPageServicePinnedPage)
+                .where({
+                    [ColNamePinPageServicePinnedPageOfUserId]: ofUserId,
+                });
+            return +(rows[0] as any)["count"];
+        } catch (error) {
+            this.logger.error("failed to create pinned page", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
     }
 
     public async getPinnedPageList(
@@ -64,23 +93,119 @@ export class PinnedPageDataAccessorImpl implements PinnedPageDataAccessor {
         offset: number,
         limit: number
     ): Promise<PinnedPage[]> {
-        throw new Error("Method not implemented.");
+        try {
+            const rows = await this.knex
+                .select()
+                .from(TabNamePinPageServicePinnedPage)
+                .where({ [ColNamePinPageServicePinnedPageOfUserId]: ofUserId })
+                .offset(offset)
+                .limit(limit)
+                .orderBy(ColNamePinPageServicePinnedPagePinTime, "desc");
+            return rows.map((row) => this.getPinnedPageFromRow(row));
+        } catch (error) {
+            this.logger.error("failed to get pinned page list", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
     }
 
-    public async getPinnedPage(id: number): Promise<PinnedPage> {
-        throw new Error("Method not implemented.");
+    public async getPinnedPage(id: number): Promise<PinnedPage | null> {
+        try {
+            const rows = await this.knex
+                .select()
+                .from(TabNamePinPageServicePinnedPage)
+                .where({ [ColNamePinPageServicePinnedPagePinnedPageId]: id });
+            if (rows.length === 0) {
+                this.logger.info("no pinned page with id found", {
+                    pinnedPageId: id,
+                });
+                return null;
+            }
+            return this.getPinnedPageFromRow(rows[0]);
+        } catch (error) {
+            this.logger.error("failed to get pinned page", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
     }
 
-    public async getPinnedPageWithXLock(id: number): Promise<PinnedPage> {
-        throw new Error("Method not implemented.");
+    public async getPinnedPageWithXLock(
+        id: number
+    ): Promise<PinnedPage | null> {
+        try {
+            const rows = await this.knex
+                .select()
+                .from(TabNamePinPageServicePinnedPage)
+                .where({ [ColNamePinPageServicePinnedPagePinnedPageId]: id })
+                .forUpdate();
+            if (rows.length === 0) {
+                this.logger.info("no pinned page with id found", {
+                    pinnedPageId: id,
+                });
+                return null;
+            }
+            return this.getPinnedPageFromRow(rows[0]);
+        } catch (error) {
+            this.logger.error("failed to get pinned page", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
     }
 
     public async updatePinnedPage(pinnedPage: PinnedPage): Promise<void> {
-        throw new Error("Method not implemented.");
+        try {
+            await this.knex
+                .table(TabNamePinPageServicePinnedPage)
+                .update({
+                    [ColNamePinPageServicePinnedPageOfUserId]:
+                        pinnedPage.ofUserId,
+                    [ColNamePinPageServicePinnedPagePinTime]:
+                        pinnedPage.pinTime,
+                    [ColNamePinPageServicePinnedPageURL]: pinnedPage.url,
+                    [ColNamePinPageServicePinnedPageScreenshotFilename]:
+                        pinnedPage.screenshotFilename,
+                    [ColNamePinPageServicePinnedPageDescription]:
+                        pinnedPage.screenshotFilename,
+                })
+                .where({
+                    [ColNamePinPageServicePinnedPagePinnedPageId]:
+                        pinnedPage.id,
+                });
+        } catch (error) {
+            this.logger.error("failed to update pinned page", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
     }
 
     public async deletePinnedPage(id: number): Promise<void> {
-        throw new Error("Method not implemented.");
+        try {
+            const deleteCount = await this.knex
+                .delete()
+                .from(TabNamePinPageServicePinnedPage)
+                .where({
+                    [ColNamePinPageServicePinnedPagePinnedPageId]: id,
+                });
+            if (deleteCount === 0) {
+                this.logger.debug("no pinned page with pinned_page_id found", {
+                    pinnedPageId: id,
+                });
+                throw new ErrorWithStatus(
+                    `no pinned page with pinned_page_id ${id} found`,
+                    status.NOT_FOUND
+                );
+            }
+        } catch (error) {
+            this.logger.error("failed to delete pinned page", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
+    }
+
+    private getPinnedPageFromRow(row: Record<string, any>): PinnedPage {
+        return new PinnedPage(
+            +row[ColNamePinPageServicePinnedPagePinnedPageId],
+            +row[ColNamePinPageServicePinnedPageOfUserId],
+            +row[ColNamePinPageServicePinnedPagePinTime],
+            row[ColNamePinPageServicePinnedPageURL],
+            row[ColNamePinPageServicePinnedPageScreenshotFilename],
+            row[ColNamePinPageServicePinnedPageDescription]
+        );
     }
 }
 
